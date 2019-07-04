b4706860e934ee0633587795ed2dd06a2246ee91
brentmorley <brentmorley75@gmail.com>
2019-07-04 13:37:18 -0600

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+// import logo from './logo.svg';
+// import initialImage from "src/images/hang0.gif";
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <img alt="test" src={"./images/hang0.gif"}/>
+    </div>
+  );
+}
+
+export default App;
